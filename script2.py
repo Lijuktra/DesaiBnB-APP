8cb8af068df293ca06fd59679770d1a14b1f0dad
lijuktra <lijuktra@yahoo.com>
2019-08-12 18:47:50 +0200

    update

---
+++ script2.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/about/')
+def about():
+    return "Hai the website from Liju"
+
+@app.route('/home/')
+def home():
+    return "Welcome to home page"
+
+
+if __name__=="__main__":
+    app.run(debug=True)
